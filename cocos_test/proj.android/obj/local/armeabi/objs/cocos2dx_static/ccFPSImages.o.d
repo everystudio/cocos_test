306d001076d78c4c882c556429d979d6bfee5b7e
mainitiamai <mainitiamai@gmail.com>
2014-03-09 15:42:14 +0900

    ディレクトリ変更(2/2)

--- cocos_test/proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /Applications/eclipse/cocos2d-x-2.1.4/projects/cocos_test/proj.android/../../../cocos2dx/ccFPSImages.c \
- /Applications/eclipse/cocos2d-x-2.1.4/projects/cocos_test/proj.android/../../../cocos2dx/ccFPSImages.h
-
-/Applications/eclipse/cocos2d-x-2.1.4/projects/cocos_test/proj.android/../../../cocos2dx/ccFPSImages.h:
